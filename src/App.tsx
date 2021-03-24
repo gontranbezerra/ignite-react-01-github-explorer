8f87c67865ad133ee35d36b91bd15ffac7bf979b
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-24 15:25:06 -0300

    1st week - ignite course

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '../src/styles/global.scss';
+
+import { RepositoryList } from './components/RepositoryList';
+
+export function App() {
+  //   throw new Error('Eita, um erro!');
+  // return <h1>Hello World</h1>;
+  return (
+    // <>
+    <RepositoryList />
+    // </>
+  );
+}
